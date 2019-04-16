64946bb00c63f4c104cd4f76d07e05273a8feadb
alexandrustana <alexandrustana2@gmail.com>
2019-04-16 23:43:11 +0300

    Fix bug in used pair arguments

--- src/upt/se/pairs/UsedArgumentsTypes.java
+++ src/upt/se/pairs/UsedArgumentsTypes.java
@@ -2,7 +2,7 @@ package upt.se.pairs;
 
 import static io.vavr.API.For;
 import static upt.se.utils.builders.ListBuilder.toList;
-import static upt.se.utils.helpers.ClassNames.isEqual;
+import static upt.se.utils.helpers.ClassNames.*;
 import static upt.se.utils.helpers.LoggerHelper.LOGGER;
 import java.util.Collections;
 import java.util.logging.Level;
@@ -14,12 +14,10 @@ import io.vavr.control.Try;
 import ro.lrg.xcore.metametamodel.Group;
 import ro.lrg.xcore.metametamodel.IRelationBuilder;
 import ro.lrg.xcore.metametamodel.RelationBuilder;
-import thesis.metamodel.entity.MArgumentType;
 import thesis.metamodel.entity.MTypePair;
 import thesis.metamodel.factory.Factory;
 import upt.se.utils.TypePair;
 import upt.se.utils.builders.GroupBuilder;
-import upt.se.utils.builders.ListBuilder;
 import upt.se.utils.store.ITypeBindingStore;
 
 @RelationBuilder
@@ -27,17 +25,11 @@ public class UsedArgumentsTypes implements IRelationBuilder<MTypePair, MTypePair
 
   @Override
   public Group<MTypePair> buildGroup(MTypePair entity) {
-    return For(getUsedArgumentsTypes(entity), getDeclaringClassesArgumentsTypes(entity))
-      .yield((usedTypes, declaringClassesArgumentTypes) -> declaringClassesArgumentTypes
-          .filter(arguments -> arguments.find(argument -> usedTypes._1.find(type -> isEqual(type, argument._1)).isDefined() ||
-                                                          usedTypes._2.find(type -> isEqual(type, argument._1)).isDefined())
-                                        .isDefined())
-          .flatMap(usedArguments -> getParametersPosition(entity)
-                                    .map(parameters -> Tuple.of(usedArguments, parameters)))
-          .map(tuple -> tuple._1.filter(argument -> tuple._2.find(parameter -> parameter._2 == argument._2)
-                                                            .isDefined())))
+    return For(getDeclaringClassesArgumentsTypes(entity), getAttributesArgumentsTypes(entity))
+      .yield((declaringClasses, attributeDeclarations) -> declaringClasses.appendAll(attributeDeclarations))
       .map(usedArguments -> usedArguments.map(arguments -> arguments.map(argument -> argument._1)))
       .map(usedArguments -> usedArguments.map(arguments -> Tuple.of(arguments.head(), arguments.tail().head())))
+      .map(pairs -> pairs.distinctBy((p1, p2) -> isEqual(p1, p2) ?  0 : 1))
       .map(pairs -> pairs.map(pair -> new TypePair(pair._1, pair._2))
                          .map(Factory.getInstance()::createMTypePair))
       .map(list -> list.asJava())
@@ -47,23 +39,6 @@ public class UsedArgumentsTypes implements IRelationBuilder<MTypePair, MTypePair
       .get(); 
   }
 
-  private Try<List<Tuple2<ITypeBinding,Integer>>> getParametersPosition(MTypePair entity) {
-    return Try.of(() -> entity.getUnderlyingObject())
-        .map(parameters -> Tuple.of(parameters, parameters.getFirst().getDeclaringClass()))
-        .map(tuple -> toList(tuple._2.getTypeParameters()).zipWithIndex()
-                                                          .filter(parameter -> isEqual(tuple._1.getFirst(), parameter._1) ||
-                                                                               isEqual(tuple._1.getSecond(), parameter._1)));
-  }
-  
-  private Try<Tuple2<List<ITypeBinding>,List<ITypeBinding>>> getUsedArgumentsTypes(MTypePair entity) {
-    return Try.of(() -> entity.getUnderlyingObject())
-      .map(parameters -> Tuple.of(parameters.getFirst(), parameters.getSecond()))
-      .map(tuple -> tuple.map(Factory.getInstance()::createMArgumentType, Factory.getInstance()::createMArgumentType))
-      .map(tuple -> tuple.map(MArgumentType::usedArgumentTypes,MArgumentType::usedArgumentTypes))
-      .map(tuple -> tuple.map(GroupBuilder::unwrap, GroupBuilder::unwrap)
-                         .map(ListBuilder::toList, ListBuilder::toList));
-  }
-  
   private Try<List<List<Tuple2<ITypeBinding,Integer>>>> getDeclaringClassesArgumentsTypes(MTypePair entity) {
     return Try.of(() -> entity.getUnderlyingObject())
       .map(parameters -> Tuple.of(parameters.getFirst(), parameters.getSecond()))
@@ -75,5 +50,14 @@ public class UsedArgumentsTypes implements IRelationBuilder<MTypePair, MTypePair
                                .map(superClass -> toList(superClass.getTypeArguments())
                                                       .zipWithIndex()));
   }
-
+  
+  private Try<List<List<Tuple2<ITypeBinding,Integer>>>> getAttributesArgumentsTypes(MTypePair entity) {
+    return Try.of(() -> entity.getUnderlyingObject())
+      .map(parameters -> Tuple.of(parameters.getFirst(), parameters.getSecond()))
+      .map(tuple -> tuple.map(Factory.getInstance()::createMArgumentType, Factory.getInstance()::createMArgumentType))
+      .map(tuple -> tuple.map(ITypeBindingStore::usagesInVariables, ITypeBindingStore::usagesInVariables))
+      .map(tuple -> toList(tuple._1).zip(tuple._2))
+      .map(list -> list.map(tuple -> List.of(tuple._1, tuple._2)
+                                         .zipWithIndex()));
+  }
 }
