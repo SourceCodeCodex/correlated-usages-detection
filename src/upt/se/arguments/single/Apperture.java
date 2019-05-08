cade2ba53ca2c9c90c99baa012bbada37f150ac9
alexandrustana <alexandrustana2@gmail.com>
2019-05-08 23:55:39 +0300

    Handle parent type found in usages

--- src/upt/se/arguments/single/Apperture.java
+++ src/upt/se/arguments/single/Apperture.java
@@ -1,8 +1,11 @@
 package upt.se.arguments.single;
 
 import static upt.se.utils.helpers.Converter.round;
+import static upt.se.utils.helpers.Equals.*;
+import java.util.List;
 import ro.lrg.xcore.metametamodel.IPropertyComputer;
 import ro.lrg.xcore.metametamodel.PropertyComputer;
+import thesis.metamodel.entity.MClass;
 import thesis.metamodel.entity.MParameter;
 
 @PropertyComputer
@@ -10,7 +13,14 @@ public class Apperture implements IPropertyComputer<String, MParameter> {
 
   @Override
   public String compute(MParameter entity) {
-    double apperture = entity.usedArgumentTypes().getElements().size() * 100d
+    List<MClass> usedTypes = entity.usedArgumentTypes().getElements();
+
+    if (usedTypes.stream().anyMatch(
+        usedType -> isEqual(usedType.getUnderlyingObject(), entity.getUnderlyingObject().getSuperclass()))) {
+      return 100 + "%";
+    }
+    
+    double apperture = usedTypes.size() * 100d
         / entity.allPossibleArgumentTypes().getElements().size();
 
     return round(apperture, 2) + " %";
