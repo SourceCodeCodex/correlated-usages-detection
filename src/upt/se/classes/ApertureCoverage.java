54ee526720f8ca8ca7ec567d40fd2dbe4d73411f
Petru Florin Mihancea <petrum@gmail.com>
2021-06-26 19:46:42 +0300

    fix the aperture coverage for classes

--- src/upt/se/classes/ApertureCoverage.java
+++ src/upt/se/classes/ApertureCoverage.java
@@ -1,6 +1,5 @@
 package upt.se.classes;
 
-import io.vavr.collection.List;
 import ro.lrg.xcore.metametamodel.IPropertyComputer;
 import ro.lrg.xcore.metametamodel.PropertyComputer;
 import thesis.metamodel.entity.MClass;
@@ -10,12 +9,8 @@ public class ApertureCoverage implements IPropertyComputer<Double, MClass> {
 
 	@Override
 	public Double compute(MClass entity) {
-		List<Double> apertures = List.ofAll(entity.typeParameterPairs().getElements())
-	        .map(parameter -> parameter.aperture());
-		if (apertures.size() == 0) {
-			return Double.NaN;
-		}
-	    return apertures.fold(0d, (elem, acc) -> elem + acc) / apertures.size();
+		return entity.typeParameterPairs().getElements().stream()
+			.mapToDouble(pair -> pair.aperture()).min().orElse(Double.NaN);
 	}
 	
 }
