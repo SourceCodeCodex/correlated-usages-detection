8b639eb33f509e6cfb816bb59adee32a6905f30f
Petru Florin Mihancea <petrum@gmail.com>
2021-06-27 11:19:06 +0300

    the meta-model is generated and thus, it must not be versioned

--- metamodel/thesis/metamodel/entity/MClassPair.java
+++
@@ -1,9 +0,0 @@
-package thesis.metamodel.entity;
-
-public interface MClassPair extends ro.lrg.xcore.metametamodel.XEntity {
-
-	@ro.lrg.xcore.metametamodel.ThisIsAProperty
-	public java.lang.String toString();
-
-	upt.se.utils.ArgumentPair getUnderlyingObject();
-}
