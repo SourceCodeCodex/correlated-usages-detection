83e040d1eb9b808a805456e862cac312d531b743
Petru Florin Mihancea <petrum@gmail.com>
2021-06-28 14:14:57 +0300

    fixes for lower bounded wildcard

--- src/upt/se/parameters/pair/UsedArgumentsTypes.java
+++ src/upt/se/parameters/pair/UsedArgumentsTypes.java
@@ -34,11 +34,8 @@ public class UsedArgumentsTypes implements IRelationBuilder<MClassPair, MParamet
 	public Group<MClassPair> buildGroup(MParameterPair entity) {
 		return Try.of(() -> entity.getUnderlyingObject())
 				.map(pair -> getInheritanceUsages(pair).appendAll(getVariablesUsages(pair)))
-				.map(usedArgumentPairs -> usedArgumentPairs.toMap(pair -> toString(pair))
-						.values()
-						.toList())
-				.map(usedArgumentPairs -> usedArgumentPairs.map(pair -> new ArgumentPair(pair._1, pair._2)))
-				.map(usedArgumentPairs -> getHierarchy(usedArgumentPairs))
+				.map(usedArgumentPairs -> getHierarchy(usedArgumentPairs, entity.getUnderlyingObject()))
+				.map(usedArgumentPairs -> usedArgumentPairs.toMap(pair -> toString(pair)).values().toList())
 				.map(usedArgumentPairs -> usedArgumentPairs.map(Factory.getInstance()::createMClassPair))
 				.onFailure(exception -> LOGGER.log(Level.SEVERE,
 						"An error occurred while trying to get all the parameters for: "
@@ -50,13 +47,12 @@ public class UsedArgumentsTypes implements IRelationBuilder<MClassPair, MParamet
 				.get();
 	}
 	
-	private Tuple2<String,Tuple2<IType,IType>> toString(Tuple2<IType,IType> pair) {
-		String key = "(" + pair._1.getFullyQualifiedName() + "," + pair._2.getFullyQualifiedName() + ")";
-		
+	private Tuple2<String,ArgumentPair> toString(ArgumentPair pair) {
+		String key = "(" + pair.getFirst().getFullyQualifiedName() + "," + pair.getSecond().getFullyQualifiedName() + ")";		
 		return Tuple.of(key, pair);
 	}
-	
-	private List<Tuple2<IType,IType>> getInheritanceUsages(ParameterPair pair) {
+
+	private List<Tuple2<ITypeBinding,ITypeBinding>> getInheritanceUsages(ParameterPair pair) {
 		ITypeBinding firstParameter = pair.getFirst();
 		ITypeBinding secondParameter = pair.getSecond();
 		List<List<ITypeBinding>> usages = InheritanceArgumentTypes.getUsages(firstParameter.getDeclaringClass());
@@ -64,10 +60,10 @@ public class UsedArgumentsTypes implements IRelationBuilder<MClassPair, MParamet
 		int firstParameterPos = InheritanceArgumentTypes.getParameterNumber(firstParameter);
 		int secondParameterPos = InheritanceArgumentTypes.getParameterNumber(secondParameter);
 		
-		return usages.map(types -> Tuple.of(analyzed(types.get(firstParameterPos)), analyzed(types.get(secondParameterPos))));
+		return usages.map(types -> Tuple.of(types.get(firstParameterPos), types.get(secondParameterPos)));
 	}	
 	
-	private List<Tuple2<IType,IType>> getVariablesUsages(ParameterPair pair) {
+	private List<Tuple2<ITypeBinding,ITypeBinding>> getVariablesUsages(ParameterPair pair) {
 		ITypeBinding firstParameter = pair.getFirst();
 		ITypeBinding secondParameter = pair.getSecond();
 		List<List<ITypeBinding>> usages = VariablesArgumentTypes.getUsages(firstParameter.getDeclaringClass());
@@ -75,46 +71,43 @@ public class UsedArgumentsTypes implements IRelationBuilder<MClassPair, MParamet
 		int firstParameterPos = VariablesArgumentTypes.getParameterNumber(firstParameter);
 		int secondParameterPos = VariablesArgumentTypes.getParameterNumber(secondParameter);
 		
-		return usages.map(types -> Tuple.of(analyzed(types.get(firstParameterPos)), analyzed(types.get(secondParameterPos))));
+		return usages.map(types -> Tuple.of(types.get(firstParameterPos), types.get(secondParameterPos)));
 	}
 	
-	private List<ArgumentPair> getHierarchy(List<ArgumentPair> pairs) {
+	private List<ArgumentPair> getHierarchy(List<Tuple2<ITypeBinding, ITypeBinding>> pairs, ParameterPair parameterPair) {
 		return pairs.flatMap(pair -> {
-			IType firstType = pair.getFirst();
-			IType secondType = pair.getSecond();
-			
-			List<IType> firstTypeHierarchy = getHierarchy(firstType);
-			List<IType> secondTypeHierarchy = getHierarchy(secondType);
-			
+			ITypeBinding firstType = pair._1;
+			ITypeBinding secondType = pair._2;
+			List<IType> firstTypeHierarchy = getHierarchy(firstType, parameterPair.getFirst());
+			List<IType> secondTypeHierarchy = getHierarchy(secondType, parameterPair.getSecond());
 			LinkedList<ArgumentPair> result = new LinkedList<>();
 			firstTypeHierarchy.forEach(first -> {
 				secondTypeHierarchy.forEach(second -> {
 					result.add(new ArgumentPair(first, second));
 				});
 			});
-			
 			return List.ofAll(result);
 		});
 	}
 		
-	private List<IType> getHierarchy(IType type) {
-		if(isAbstract(type)) {
-			try {
-				return List.of(type.newTypeHierarchy(NULL_PROGRESS_MONITOR).getAllSubtypes(type)).filter(tpe -> !isAbstract(tpe));
-			} catch (JavaModelException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	private List<IType> getHierarchy(ITypeBinding argument, ITypeBinding parameter) {
+		try {
+			IType tmp;
+			if (argument.isWildcardType()) {
+				if (argument.isUpperbound()) {
+					tmp = (IType) argument.getBound().getJavaElement();
+				} else {
+					tmp = (IType) parameter.getTypeBounds()[0].getJavaElement();					
+				}
+			} else {
+				tmp = (IType) argument.getJavaElement();
 			}
+			return List.of(tmp.newTypeHierarchy(NULL_PROGRESS_MONITOR).getAllSubtypes(tmp))
+					.append(tmp).filter(tpe -> !isAbstract(tpe));
+		} catch (JavaModelException e) {
+				e.printStackTrace();
 		}
-		return List.of(type);
-	}
-
-	private static IType analyzed(ITypeBinding binding) {
-		if (binding.isWildcardType()) {
-			return (IType) binding.getBound().getJavaElement();
-		} else {
-			return (IType) binding.getJavaElement();
-		}
+		return List.of();
 	}
 
 }
